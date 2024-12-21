7eb939139a80e5eb5526c5307c368bead1ebac8f
Bruno Santos <brunoadsjp@gmail.com>
2024-12-20 22:56:53 -0300

    iniciando a implementacao de produtos

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface IProduto {
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+}
 @Component({
     selector: 'app-produtos',
     templateUrl: './produtos.component.html',
@@ -7,5 +13,18 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class ProdutosComponent {
-
+    produtos: IProduto[] = [
+        {
+            name: 'Naked Cake 30cm',
+            description: 'Naked Cake com massa amanteigada, recheio ninho com morango',
+            price: 95.00,
+            image: 'assets/images/naked_cake_30cm.jpg'
+        },
+        {
+            name: 'Bolo caseiro 30cm',
+            description: 'Diversos sabores',
+            price: 28.00,
+            image: 'assets/images/bolo_caseiro_30cm.jpg'
+        }
+    ]
 }
